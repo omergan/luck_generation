9ba1e7d01a45d18ab36bace9877614b82257f803
RIOT\Omerga <ganon136@gmail.com>
2020-01-19 14:21:57 +0200

    implemented use of the new class in luck generation

--- measuring_luck_generation/generating_luck.py
+++ measuring_luck_generation/generating_luck.py
@@ -1,11 +1,11 @@
 import database_api
 from enums import Strength
-from measuring_tie_strength import measure_tie_strength
-import logging
-logging.basicConfig(level=logging.INFO)
+from measuring_tie_strength import measure_tie_strength as tsm
+from utils import Logger
+logger = Logger()
 
 def generating_luck(user, context):
-    logging.info(f'Generating_luck for given user : {user} in context of : {context}')
+    logger.luck(f'Generating_luck for given user : {user} in context of : {context}')
     # Generate set from datamuse
     strong_keywords = database_api.get_datamuse_set(context, Strength.STRONG.value)
     weak_keywords = database_api.get_datamuse_set(context, Strength.WEAK.value)
@@ -22,8 +22,9 @@ def generating_luck(user, context):
     # Dictionary [{username, strong tie score, weak tie score}] of candidate and their score
     ranking = []
 
-    strong_tie_score = measure_tie_strength.measure_tie_strength(user, candidates[0], strong_keywords, context)
-    logging.info(f'Measure tie strength for strong connection return : {strong_tie_score} for candidate {candidates[0]}')
+    tie_streangth_tools = tsm.TieStrengthTool()
+    strong_tie_score = tie_streangth_tools.measure_tie_strength(user, candidates[0], strong_keywords, context)
+    logger.luck(f'Measure tie strength for strong connection return : {strong_tie_score} for candidate {candidates[0]}')
 
     # Create queue with strong measured
     # for candidate in candidates:
