3326c0d87c03b3f9642d51d32fe1fba6e0528360
RIOT\Omerga <ganon136@gmail.com>
2020-02-27 00:16:38 +0200

    fix followers of followers

--- measuring_luck_generation/generating_luck.py
+++ measuring_luck_generation/generating_luck.py
@@ -105,15 +105,16 @@ class LuckGenerator:
         followers = self.get_candidates(self.strict_set, customer_profile)
         for i, follower in enumerate(followers):
             logger.debug(f'Scraping a customer direct follower {follower["username"]}')
-            if len(database_api.get_all_tweets_by_username(follower['username'])) < 200:
+            if len(database_api.get_all_tweets_by_username(follower['username'])) < 30:
                 twint_api.get_profile_by_username(follower['username'])
                 twint_api.get_tweets_by_username(follower['username'], self.limit)
 
             follower_profile = database_api.get_profile(follower['username'])
+            followers_of_followers = twint_api.get_followers(follower_profile[0], 5)
             followers_of_followers = self.get_candidates(self.strict_set, follower_profile)
             for j, x in enumerate(followers_of_followers):
                 logger.debug(f'{i}.{j} : {x["username"]}')
-                if len(database_api.get_all_tweets_by_username(x['username'])) < 200:
+                if len(database_api.get_all_tweets_by_username(x['username'])) < 30:
                     twint_api.get_profile_by_username(x['username'])
                     twint_api.get_tweets_by_username(x['username'], self.limit)
 
