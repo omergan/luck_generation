a28061f266469a6f5fe00a044937084cce1224c1
RIOT\Omerga <ganon136@gmail.com>
2020-05-05 15:39:21 +0300

    implement user and tweet classes

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ from measuring_tie_strength import measure_tie_strength as tsm
 import twint_api
 from measuring_luck_generation import datamuse_api
 import database_api
-
+from pprint import pprint
+from measuring_tie_strength.models import User, Tweet
 LIMIT = 1
 
 from utils import Logger
 logger = Logger()
 
 if __name__ == '__main__':
-    logger.debug("Starting main program!\n")
-    luck_generator = LG.LuckGenerator(is_online=False, limit=LIMIT)
-    # luck_generator.generating_luck("Charliedysonrec", "Looking for a software engineering job")
-    luck_generator.scrap("DevProtege")
-    # luck_generator.draw_mosaic(None)
-    logger.debug("\nEnding main program!")
+    user = User("Charliedysonrec")
+    for tweet in user.get_tweets():
+        print(tweet)
+
+
+    # logger.debug("Starting main program!\n")
+    # luck_generator = LG.LuckGenerator(is_online=False, limit=LIMIT)
+    # # luck_generator.generating_luck("Charliedysonrec", "Looking for a software engineering job")
+    # luck_generator.scrap("DevProtege")
+    # # luck_generator.draw_mosaic(None)
+    # logger.debug("\nEnding main program!")
--- measuring_tie_strength/models.py
+++ measuring_tie_strength/models.py
@@ -1,24 +1,51 @@
-
+import database_api as db
 
 class User(object):
-    def __init__(self, user_id):
+    def __init__(self, username):
         """ Initialize a user instance with data """
-        pass
+        user_profile = db.get_profile(username)
+        self.id = user_profile[0]
+        self.name = user_profile[2]
+        self.username = user_profile[3]
+        self.bio = user_profile[4]
+        self.location = user_profile[5]
+        self.url = user_profile[6]
+        self.join_date = user_profile[7]
+        self.join_time = user_profile[8]
+        self.tweet_count = user_profile[9]
+        self.following_count = user_profile[10]
+        self.followers_count = user_profile[11]
+        self.likes_count = user_profile[12]
+        self.verified = user_profile[15]
 
 
     def get_tweets(self):
         """ Retrieve user tweets """
-        tweets = []
-        return tweets
+        tweets = db.get_all_tweets_by_username(self.username)
+        tweets_obj = []
+        for tweet in tweets:
+            tweets_obj.append(Tweet(tweet))
+        return tweets_obj
 
     def __str__(self):
-        pass
+        return f'{self.id} : {self.name}'
 
 class Tweet(object):
-    def __init__(self, tweet_id):
-        pass
+    def __init__(self, tweet):
+        self.id = tweet[0]
+        self.tweet = tweet[2]
+        self.conversation_id = tweet[3]
+        self.date = tweet[5]
+        self.time = tweet[6]
+        self.timezone = tweet[7]
+        self.replies_count = tweet[9]
+        self.likes_count = tweet[10]
+        self.retweets_count = tweet[11]
+        self.user_id = tweet[12]
+        self.username = tweet[14]
+        self.link = tweet[16]
 
     def __str__(self):
-        pass
+        return f'{self.date} {self.username} : {self.tweet}'
 
 
