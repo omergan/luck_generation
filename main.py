873c64079202e52da0c738e029f22a75e3174214
RIOT\Omerga <ganon136@gmail.com>
2020-05-06 19:04:29 +0300

    implemented network plot

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import twint_api
 from measuring_luck_generation import datamuse_api
 import database_api
 from pprint import pprint
+import numpy as np
+import matplotlib.pyplot as plt
 from measuring_tie_strength.models import User, Tweet
 from measuring_tie_strength.graph import Network
 LIMIT = 1
@@ -11,13 +13,62 @@ LIMIT = 1
 from utils import Logger
 logger = Logger()
 
+
+import networkx as nx
+def map_connections(graph):
+    key_indexes = {}
+    value_indexes = {}
+    i = 0
+    j = 0
+    for key in graph:
+        if key not in key_indexes:
+            key_indexes[key] = i
+            i += 1
+        for value in graph[key]:
+            if value not in value_indexes:
+                value_indexes[value] = j
+                j += 1
+
+    map = np.zeros((len(key_indexes), len(value_indexes)))
+    for key in graph:
+        for value in graph[key]:
+            map[key_indexes[key]][value_indexes[value]] = 1
+
+    plt.subplot(111), plt.imshow(map, cmap='gray'), plt.title("Tightness")
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+def count_followers(graph):
+    for key in graph:
+        print(f'{key} : {len(graph[key])}')
+
+def networkx_things():
+    # with open('datasets/edge_list.txt') as f:
+    #     for row in f:
+    #         print(row)
+    g = nx.read_edgelist('datasets/edge_list.txt', create_using=nx.Graph(), nodetype=str)
+    nx.draw(g)
+    plt.show()
+    # f = open("datasets/edge_list.txt", "w")
+    # i=1
+    # for key in graph:
+    #     for value in graph[key]:
+    #         f.write('{} {}\n'.format(key, value))
+    #         print(f'{i} {key} {value}')
+    #         i += 1
+    # f.close()
+    # pprint(graph)
+
+
 if __name__ == '__main__':
     user = User("Charliedysonrec")
-    for tweet in user.get_tweets():
-        print(tweet)
-
     network = Network()
-
+    graph = network._graph
+    # count_followers(graph)
+    # map_connections(graph)
+    # networkx_things()
+    nx.draw(graph)
+    plt.show()  # display
     # logger.debug("Starting main program!\n")
     # luck_generator = LG.LuckGenerator(is_online=False, limit=LIMIT)
     # # luck_generator.generating_luck("Charliedysonrec", "Looking for a software engineering job")
--- measuring_tie_strength/graph.py
+++ measuring_tie_strength/graph.py
@@ -1,37 +1,13 @@
 import pprint
 from collections import defaultdict
+import networkx as nx
 import database_api as db
 
 class Network(object):
     """ Graph data structure, undirected by default. """
     def __init__(self, directed=True):
-        self._graph = defaultdict(set)
-        self._directed = directed
-        connections = self.load_connections()
-        self.add_connections(connections)
-
-    def add_connections(self, connections):
-        """ Add connections (list of tuple pairs) to graph """
-        for node1, node2 in connections:
-            self.add(node1, node2)
-
-    def add(self, node1, node2):
-        """ Add connection between node1 and node2 """
-        self._graph[node1].add(node2)
-        if not self._directed:
-            self._graph[node2].add(node1)
-
-    def remove(self, node):
-        """ Remove all references to node """
-        for n, cxns in self._graph.items():
-            try:
-                cxns.remove(node)
-            except KeyError:
-                pass
-        try:
-            del self._graph[node]
-        except KeyError:
-            pass
+        self._graph = nx.Graph()
+        self._graph.add_edges_from(self.load_connections())
 
     def load_connections(self):
         connections = db.get_all_connections()
