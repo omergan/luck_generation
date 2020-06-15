ad01a931f9bae8623f90611db73bc4ee79d331f6
asafjo23 <asafjo23@gmail.com>
2020-06-15 21:06:04 +0300

    CLI implements starts

--- main.py
+++ main.py
@@ -44,35 +44,35 @@ def count_followers(graph):
         print(f'{key} : {len(graph[key])}')
 
 if __name__ == '__main__':
-    users = []
-    users.append(User("Charliedysonrec"))  # 800589492
-    users.append(User("MizrahiMichael"))  # 2993950570
-    users.append(User("LukeMorton"))
-    user = users[0]
+    # users = []
+    # users.append(User("Charliedysonrec"))  # 800589492
+    # users.append(User("MizrahiMichael"))  # 2993950570
+    # users.append(User("LukeMorton"))
+    # user = users[0]
     # tsmtool = tsm.TieStrengthTool(is_online=False, limit=20, username=user.username)
 
 
-    logger.debug("Starting main program!\n")
-    df = pd.read_excel('new - Charliedysonrec.xlsx')
-    luck = list(df.T.to_dict().values())
-    user_dict = tsmtool.network.user_dict
+    # logger.debug("Starting main program!\n")
+    # df = pd.read_excel('new - Charliedysonrec.xlsx')
+    # luck = list(df.T.to_dict().values())
+    # user_dict = tsmtool.network.user_dict
+    #
+    # orig_list = tsmtool.network.graph
+    # len(orig_list)
+    # filter = filter_topology(luck, orig_list, user, 5, user_dict)
+    # # filter = filter_luck(luck, orig_list, user, 3.3, user_dict)
+    # tsmtool.network.create_subgraph(filter)
+    # color_map = map_colors(luck, filter, user.id, user_dict)
+    # size_map = map_size(luck, filter, user.id, user_dict)
+    # tsmtool.network.draw(list(filter), color_map, size_map)
+    #
+    # logger.debug("\nEnding main program!")
 
-    orig_list = tsmtool.network.graph
-    len(orig_list)
-    filter = filter_topology(luck, orig_list, user, 5, user_dict)
-    # filter = filter_luck(luck, orig_list, user, 3.3, user_dict)
-    tsmtool.network.create_subgraph(filter)
-    color_map = map_colors(luck, filter, user.id, user_dict)
-    size_map = map_size(luck, filter, user.id, user_dict)
-    tsmtool.network.draw(list(filter), color_map, size_map)
-
-    logger.debug("\nEnding main program!")
 
 
-
-    # luck_generator = LG.LuckGenerator(is_online=False, limit=LIMIT)
-    # username = "Charliedysonrec"
-    # luck_generator.generating_luck(username, "Looking for a software engineering job")
+    luck_generator = LG.LuckGenerator(is_online=False, limit=LIMIT)
+    username = "LukeMorton"
+    luck_generator.generating_luck(username, "Looking for a software engineering job")
     # luck = luck_generator.luck
     # luck_generator.generating_luck("Charliedysonrec", "Looking for a software engineering job")
     # luck_generator.scrap("DevProtege")
