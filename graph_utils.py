c17ddcef8a094ea04282d89b1670a9e462a7c9ce
RIOT\Omerga <ganon136@gmail.com>
2020-06-16 22:42:46 +0300

    Added label mapping to the new branch

--- graph_utils.py
+++ graph_utils.py
@@ -1,21 +1,26 @@
 def map_colors(luck, nodelist, center_id, user_dict):
     color_map = []
-    for node in nodelist:
+    count = 0
+    th = int(0.05 * len(nodelist))
+    luck_threshold = get_luck_threshold(th, nodelist)
+
+    for i, node in enumerate(nodelist):
         luck_value = 0
         for x in luck:
             if x['username'] == user_dict[node].username:
                 luck_value = x['luck']
 
         # Set Color
-        luck_threshold = get_luck_threshold(10, luck)  # top 10 will be marked as red
         if node == center_id:
             color_map.append('#00FF00')
         elif luck_value >= luck_threshold:
-            color_map.append('#FF5733')
+            if count < th / 2:
+                color_map.append('#FF5733') # orange
+            else:
+                color_map.append('#D40883') # pink
+            count += 1
         elif luck_threshold > luck_value > 2.5:
             color_map.append('#F6FF33')
-        # elif 2.5 >= luck_value > 2:
-        #     color_map.append('#90FF33')
         else:
             color_map.append('#3390FF')
     return color_map
@@ -29,6 +34,19 @@ def map_size(luck, nodelist, center_id, user_dict):
                 size = 150
         size_map.append(size)
     return size_map
+
+def map_labels(luck, nodelist, center_id, user_dict):
+    label_map = {}
+    for node in nodelist:
+        label = ""
+        if node == center_id:
+            label = user_dict[node].username
+        else:
+            for x in luck:
+                if x['username'] == user_dict[node].username and x['luck'] > 0:
+                    label = x['username']
+        label_map[node] = label
+    return label_map
 
 def get_topology(luck, username):
     for x in luck:
