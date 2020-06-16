41a16fab4176483ac64f8e1481cbca6a66f2d9d6
asafjo23 <asafjo23@gmail.com>
2020-06-16 23:27:17 +0300

    Interface works starts

--- cli/modules/interface.py
+++ cli/modules/interface.py
@@ -7,28 +7,30 @@ from cli.modules.commands import Commands
 from PyInquirer import prompt
 import cli.modules.cli_constants as constants
 
-
-def run():
-    while True:
-        answers = prompt(constants.cli_instructions, style=constants.cli_style)
-        if answers['instruction'] == 'Exit':
-            break
-        if answers['instruction'] == 'Generating luck: Run simulation':
-            print("Generating luck: Run simulation")
-            continue
-        if answers['instruction'] == 'Build entire graph':
-            print("Build entire graph")
-            continue
-        if answers['instruction'] == 'Build sub graph':
-            print("Build sub graph")
-            continue
-
-
 class Interface:
 
     def __init__(self, commands: Commands):
         try:
             self.commands = commands
-            run()
+            self.run()
         except NoConsoleScreenBufferError:
             click.echo("No Windows console found. Are you running cmd.exe?")
+
+    def run(self):
+        while True:
+            answers = prompt(constants.cli_instructions, style=constants.cli_style)
+            if answers['instruction'] == 'Exit':
+                break
+            if answers['instruction'] == 'Generating luck: Run simulation':
+                x = input("Run online? Y/N ")
+                while x != 'Y' and x != 'N':
+                    x = input("Run online? Y/N ")
+                online = True if x == 'Y' else False
+                self.commands.run_generating_luck_simulation(online=online)
+                continue
+            if answers['instruction'] == 'Build entire graph':
+                print("Build entire graph")
+                continue
+            if answers['instruction'] == 'Build sub graph':
+                print("Build sub graph")
+                continue
