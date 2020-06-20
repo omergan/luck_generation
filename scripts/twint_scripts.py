e489e072695b3d46bf15f966436048f73fd7da81
asafjo23 <asafjo23@gmail.com>
2020-06-20 18:32:34 +0300

    fix scrape function

--- scripts/twint_scripts.py
+++ scripts/twint_scripts.py
@@ -1,8 +1,10 @@
 import twint_api
 import database_api
+from measuring_tie_strength.models import User
 from utils import Logger
 
 logger = Logger()
+MAX_DEPTH = 6
 
 # Scrape only not existing users, or those who we could not scrape enough data
 def need_to_be_scrap(self, username) -> bool:
@@ -10,24 +12,23 @@ def need_to_be_scrap(self, username) -> bool:
     hasEnoughTweets = len(database_api.get_all_tweets_by_username(username)) > 30
     return isNotExist or not hasEnoughTweets
 
-def scrap(self, username):
-    while True:
-        try:
-            if database_api.get_profile(username) is None:
-                twint_api.get_profile_by_username(username)
-            customer_profile = database_api.get_profile(username)
-            twint_api.get_followers(username, self.limit)
-            followers = self.get_candidates(self.strict_set, customer_profile)
-            for i, follower in enumerate(followers):
-                logger.debug(f'Scraping a customer direct follower {follower["username"]}')
-                if need_to_be_scrap(self, follower['username']):
-                    twint_api.get_profile_by_username(follower['username'])
-                    twint_api.get_tweets_by_username(follower['username'], self.limit)
+def scrap(self, username, depth=0):
+    if depth == MAX_DEPTH:
+        return
+    if need_to_be_scrap(self, username):
+        twint_api.get_profile_by_username(username)
+    user = User(username)
+    twint_api.get_followers(username, self.limit)
+    followers_ids = database_api.get_all_followers_ids(user.id)
+    for i, follower_id in enumerate(followers_ids):
+        follower = User(follower_id, is_id=True)
+        logger.debug(f'Scraping a customer direct follower {follower.username}')
+        if need_to_be_scrap(self, follower.username):
+            twint_api.get_profile_by_username(follower.username)
+            twint_api.get_tweets_by_username(follower.username, self.limit)
 
-            # Recurrence on followers
-            for follower in followers:
-                scrap(self, follower["username"])
-
-        except Exception:
-            print(Exception)
+    # Recurrence on followers
+    for follower_id in followers_ids:
+        follower = User(follower_id, is_id=True)
+        scrap(self, follower.username, depth+1)
 
